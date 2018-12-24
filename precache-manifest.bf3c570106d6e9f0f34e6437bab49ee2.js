e36f8d364b5f508b8c79de462fd6eb7b71d2c824
L'huillier Thomas <tlhuillier@202-ecommerce.com>
2018-12-25 00:41:06 +0100

    Updates

---
+++ precache-manifest.bf3c570106d6e9f0f34e6437bab49ee2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d1a1eebc9b5c85bbcb8a",
+    "url": "/react-snake/static/js/runtime~main.d1a1eebc.js"
+  },
+  {
+    "revision": "8083d4a257978d326a6e",
+    "url": "/react-snake/static/js/main.8083d4a2.chunk.js"
+  },
+  {
+    "revision": "10360145f9b84d9aa741",
+    "url": "/react-snake/static/js/1.10360145.chunk.js"
+  },
+  {
+    "revision": "8083d4a257978d326a6e",
+    "url": "/react-snake/static/css/main.5df75d36.chunk.css"
+  },
+  {
+    "revision": "5e2cbc599e9190bf6940bc681537896d",
+    "url": "/react-snake/index.html"
+  }
+];
